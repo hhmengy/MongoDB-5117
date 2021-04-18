88be39179d33c9100cfc73c602f9badc1394a139
Andrew McCullough <39598957+hhmengy@users.noreply.github.com>
2021-04-18 12:47:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, g, redirect, url_for, jsonify
-from db import cluster
+from db import coll
 app = Flask(__name__)
 @app.route('/')
 def addInfo():
@@ -7,27 +7,27 @@ def addInfo():
 
 @app.route('/seeInfo', methods=['GET'])
 def seeInfo():
-    info = cluster.find({})
+    coll = cluster.find({})
     print(info)
     return render_template("info.html",info=info)
 @app.route('/postInfo', methods=['POST'])
 def newInfo():
     name = request.form.get("name", "unnamed")
     email = request.form.get("email", "none")
-    cluster.insert_one({"name": name, "email": email})
+    coll.insert_one({"name": name, "email": email})
     
     return redirect(url_for('seeInfo'))
 
 @app.route('/deleteBob')
 def deleteBob():
-    cluster.delete_many({"name": "Bob Saget"})
+    coll.delete_many({"name": "Bob Saget"})
 
     return redirect(url_for('seeInfo'))
 
 
 @app.route("/test")
 def test():
-    cluster.insert_one({"name": "HANNJNNNK"})
+    coll.insert_one({"name": "HANNJNNNK"})
     return "Connected to the data base!"
 if __name__ == '__main__':
     app.run(port=8000)
